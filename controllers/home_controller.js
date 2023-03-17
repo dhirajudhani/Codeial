19cff6d966b0a059ef872b059bd510ce3355a235
dhirajudhani <dhirajudhani870@gmail.com>
2023-03-17 09:39:29 +0530

    update : added db relation
    created  and displayed post , comments and learned populating and nested populating

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,26 @@
+const Post = require('../models/post');
+const { populate } = require('../models/user');
+
 module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id',25);
-    return res.render('home',{
-       title:"Home"
-    });
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
+
+    //populate or show user for each post
+    Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    })
+    .exec(function(err,posts){
+        return res.render('home',{
+            title:"Codieal | Home",
+            posts:posts
+         });
+    })
+    
 };
+
+// module.exports.actionName = function(req,res){}
